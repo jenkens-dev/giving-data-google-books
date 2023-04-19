3845b079ae098af81c3bb26bb576d7fee950cad9
Jen Kennedy <jenken131@gmail.com>
2023-04-19 14:16:36 -0700

    fixed prettier settings and formatted on save, added check for bookData to display fallback state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes?q=";
 
@@ -13,44 +13,59 @@ interface BookData {
     infoLink: string;
     imageLinks?: {
       thumbnail: string;
-    }
+    };
     publishedDate: string;
-  }
+  };
 }
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState("");
-  const [bookData, setBookData] = useState<BookData[]>([])
+  const [bookData, setBookData] = useState<BookData[]>([]);
 
-  const handleSearchSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSearchSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
     event.preventDefault();
-    await fetch(`${BOOKS_API_URL}${searchValue}&key=${process.env.NEXT_PUBLIC_API_KEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      setBookData(data.items)
-    })
-  }
+    await fetch(
+      `${BOOKS_API_URL}${searchValue}&key=${process.env.NEXT_PUBLIC_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setBookData(data.items);
+      });
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(event.target.value)
-  }
+    setSearchValue(event.target.value);
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <form onSubmit={handleSearchSubmit}>
-        <label>Search:
-          <input type="search" value={searchValue || ""} required placeholder="Book keyword" onChange={handleInputChange}/>
-        </label>
-        <button type="submit">Search</button>
+        <div>
+          <label className="sr-only" htmlFor="bookSearch">
+            Search books:
+          </label>
+          <input
+            type="search"
+            id="bookSearch"
+            value={searchValue || ""}
+            required
+            placeholder="Search books..."
+            onChange={handleInputChange}
+          />
+          <button type="submit">Search</button>
+        </div>
       </form>
       <div>
-        {bookData.map((book) => {
-          return (
-              <div key={book.id}>{book.volumeInfo.title}</div>
-            );
+        {bookData.length > 0 ? (
+          bookData.map((book) => {
+            return <div key={book.id}>{book.volumeInfo.title}</div>;
           })
-        }
+        ) : (
+          <div>Search to find books</div>
+        )}
       </div>
     </main>
-  )
+  );
 }
