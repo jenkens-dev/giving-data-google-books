07d1d8a18ee9dd0f060d275e37aace6e1cfa0f61
Jen Kennedy <jenken131@gmail.com>
2023-04-19 14:49:25 -0700

    added comment link for next image un-configured host issue

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    // next.js documentation to explain un-configured host with next/image: https://nextjs.org/docs/messages/next-image-unconfigured-host
     remotePatterns: [
       {
         protocol: "http",
