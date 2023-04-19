f9c66acd487f564b0e27bfbaca44517a95e18ad9
Jen Kennedy <jenken131@gmail.com>
2023-04-19 10:50:41 -0700

    created starter form and input, associated handle functions for the form and search bar that interact with google books api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
+
+const BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const results = await fetch(`${BOOKS_API_URL}${searchValue}&key=${process.env.NEXT_PUBLIC_API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+    })
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Insert books here!
+      <form onSubmit={handleSearchSubmit}>
+        <label>Search:
+          <input type="search" value={searchValue || ""} required placeholder="Book keyword" onChange={handleInputChange}/>
+        </label>
+        <button type="submit">Search</button>
+      </form>
     </main>
   )
 }
