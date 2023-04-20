0eb0fbcaf22762ba7bca2fa0795cda555c9164d3
Jen Kennedy <jenken131@gmail.com>
2023-04-20 16:41:18 -0700

    updated bookcard css and added bottom section to better showcase title and author name

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -29,7 +29,7 @@ export default function BookCard({
 
   return (
     <div
-      className="flex flex-col justify-center items-center w-1/2 rounded-sm border border-stone-500 hover:border-stone-900 bg-white p-4 cursor-pointer"
+      className="relative flex flex-col items-center w-1/2 rounded-sm border border-stone-500 hover:border-stone-900 bg-white cursor-pointer p-2"
       onClick={handleClick}
     >
       <Image
@@ -37,11 +37,14 @@ export default function BookCard({
         alt={`Thumbnail picture of ${title}`}
         width="200"
         height="300"
+        className=""
       />
-      <p>{title}</p>
-      {authors.map((author) => {
-        return <p key={author}>{author}</p>;
-      })}
+      <div className="w-full bg-gray-50 absolute bottom-0 border-t border-stone-500 pl-4 py-2">
+        <p>{title}</p>
+        {authors.map((author) => {
+          return <p key={author}>{author}</p>;
+        })}
+      </div>
     </div>
   );
 }
