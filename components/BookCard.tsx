087fc103339188f74393de94f568ad7447e44b3e
Jen Kennedy <jenken131@gmail.com>
2023-04-20 13:53:05 -0700

    updated css for outer card for better UX, hover and pointer, and added click handler to push to book detail page

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,15 +1,33 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface BookCardProps {
   thumbnail: string;
   title: string;
   authors: string[];
+  id: string;
 }
 
-export default function BookCard({ thumbnail, title, authors }: BookCardProps) {
-  console.log(title, authors);
+export default function BookCard({
+  thumbnail,
+  title,
+  authors,
+  id,
+}: BookCardProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push({
+      pathname: `/book/${id}`,
+      query: { title, thumbnail, authors },
+    });
+  };
+
   return (
-    <div className="w-1/2 rounded-sm border border-stone-600 bg-white p-4 relative">
+    <div
+      className="w-1/2 rounded-sm border border-stone-500 hover:border-stone-900 bg-white p-4 relative cursor-pointer"
+      onClick={handleClick}
+    >
       <Image
         src={thumbnail}
         alt={`Thumbnail picture of ${title}`}
